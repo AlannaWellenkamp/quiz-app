6b721a4f3ef54d792d38608ab2f6ca1826781ba4
Alanna Wellenkamp <actuallyalanna@gmail.com>
2020-10-02 13:17:01 -0700

    added basic function structure

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-/**
- * Example store structure
- */
+
 const store = {
-    // 5 or more questions are required
     questions: [
       {
         question: 'What color is broccoli?',
@@ -28,9 +25,96 @@ const store = {
     quizStarted: false,
     questionNumber: 0,
     score: 0
-  };
+};
+
+function render(){
+  //render relevant html
+  const currentRender = generateHtml(store);
+
+  $('main').html(currentRender);
+}
+
+function generateHtml(store){
+  //determine which element to create
+  //if quiz started ! generateBeginElement
+  if (store.quizStarted === false) {
+    return generateBeginElement();
+  }
+  if ((store.questionNumber) === store.questions.length) {
+    return generateEndElement();
+  }
+  else {
+    return generateQuestionElement();
+  }
   
-  /**
+  //if quiz end generateEndElement
+  //else generateQuestionElement
+}
+
+function generateBeginElement(){
+  return `<p>Hi there, ready for a quiz on some animal trivia?</p>
+  <form>
+    <button>I'm ready!</button>
+  </form>`
+
+}
+
+function generateEndElement(){
+  return `<p>wanna play again?</p>
+  <form>
+    <button>I'm ready!</button>
+  </form>`
+
+}
+
+function generateQuestionElement(question){
+  //produce relevant html for question
+  return `<div>
+    <div class="question group">
+    <form class="item">
+        <h2>Question Here</h2>
+         <input type="radio" name="question-1-response" id="question-1-a" value="a"/>
+         <label for="a">a) response a</label><br>
+         <input type="radio" name="question-1-response" id="question-1-b" value="b"/>
+         <label for="b">a) response b</label><br>
+         <input type="radio" name="question-1-response" id="question-1-a" value="c"/>
+         <label for="c">a) response c</label><br>
+         <input type="radio" name="question-1-response" id="question-1-a" value="d"/>
+         <label for="d">a) response d</label><br>
+          <button type="submit">Submit</button>
+      </form>   
+      <img src="images/cat.jpg" alt="striped cat sitting with ears down" class="item">
+    </div>
+  </div>`;
+}
+
+function handleBeginQuiz(){
+  //listen for and enact begin quiz
+}
+
+function handleQuestionResponse(){
+  //listen for question answer submit
+}
+
+function responseFeedback(){
+  //offer responsive feedback to answer submitted
+}
+
+function handleNextQuestion(){
+  //listen for next question click
+}
+
+function quizEnd(){
+  //render score and present with option to restart
+}
+
+
+
+
+
+
+
+/**
    * 
    * Technical requirements:
    * 
@@ -55,4 +139,11 @@ const store = {
   
   /********** EVENT HANDLER FUNCTIONS **********/
   
-  // These functions handle events (submit, click, etc)
+// These functions handle events (submit, click, etc)
+
+function quizApp() {
+  render();
+  
+}
+
+$(quizApp);
